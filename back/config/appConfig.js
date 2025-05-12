a588facd08909bf271211d3038f1d33fafc9eb62
EC2 Default User <ec2-user@ip-172-31-1-64.ap-southeast-2.compute.internal>
2025-05-12 00:11:31 +0000

    Add the ability to work with multiple Gigwa servers

--- back/config/appConfig.js
+++ back/config/appConfig.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  gigwaServer: process.env.GIGWA_SERVER,
+  gigwaServers: JSON.parse(process.env.GIGWA_SERVERS || "{}"),
   germinateServer: process.env.GERMINATE_SERVER,
   genolinkServer: process.env.GENOLINK_SERVER,
   genesysServer: process.env.GENESYS_SERVER,
-  genolinkServerPort: process.env.GENOLINK_SERVER_PORT || 3000,
+  genolinkServerPort: process.env.GENOLINK_SERVER_PORT || 4000,
 };
