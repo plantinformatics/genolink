782cd60edf330c5b2ddef6efca284c08e3527e18
EC2 Default User <ec2-user@ip-172-31-1-64.ap-southeast-2.compute.internal>
2025-05-12 00:12:01 +0000

    Add the ability to work with multiple Gigwa servers

--- front/src/config/apiConfig.js
+++ front/src/config/apiConfig.js
@@ -1,3 +1,3 @@
 export const genolinkServer = import.meta.env.VITE_GENOLINK_SERVER;
 export const genesysServer = import.meta.env.VITE_GENESYS_SERVER;
-
+export const platforms = import.meta.env.VITE_PLATFORM?.split(",") || ["Gigwa"];
