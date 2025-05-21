088ae81b4c882665fcaa20f3bc31b43cc3d7a200
EC2 Default User <ec2-user@ip-172-31-1-64.ap-southeast-2.compute.internal>
2025-05-21 05:30:48 +0000

    Add unit test for sampleNameToAccession module

---
+++ back/tests/unit/sampleNameToAccession.test.js
@@ -0,0 +1,24 @@
+const sampleNameToAccession = require("../../utils/sampleNameToAccession");
+
+describe("sampleNameToAccession", () => {
+  it("parses sample name correctly", () => {
+    expect(sampleNameToAccession("abc123xyz")).toBe("ABC 123 XYZ");
+    expect(sampleNameToAccession("sample99end")).toBe("SAMPLE 99 END");
+    expect(sampleNameToAccession("test1abc")).toBe("TEST 1 ABC");
+  });
+
+  it("throws error on unexpected format", () => {
+    expect(() => sampleNameToAccession("abc")).toThrow(
+      "String format is not as expected."
+    );
+    expect(() => sampleNameToAccession("123abc")).toThrow(
+      "String format is not as expected."
+    );
+    expect(() => sampleNameToAccession("abc123")).toThrow(
+      "String format is not as expected."
+    );
+    expect(() => sampleNameToAccession("")).toThrow(
+      "String format is not as expected."
+    );
+  });
+});
