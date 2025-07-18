fe8f73153fe497f24ab92eb59ea35e18e2b96ce0
EC2 Default User <ec2-user@ip-172-31-1-64.ap-southeast-2.compute.internal>
2025-07-18 02:47:44 +0000

    migrate PositionRangeFilter styles to CSS module

--- front/src/components/genotype/filters/PositionRangeFilter.jsx
+++ front/src/components/genotype/filters/PositionRangeFilter.jsx
@@ -1,23 +1,23 @@
+import styles from "./PositionRangeFilter.module.css";
+
 const PositionRangeFilter = ({ posStart, setPosStart, posEnd, setPosEnd }) => {
   return (
-    <div style={{ display: "flex", gap: "10px", alignItems: "center", margin: "10px 0 10px 0" }}>
+    <div className={styles.container}>
       <input
         id="posStart"
         type="text"
-        className="form-control"
+        className={`${styles.formControl} ${styles.input} ${styles.posWidth}`}
         value={posStart}
         onChange={(e) => setPosStart(e.target.value)}
         placeholder="Start Position"
-        style={{ width: "280px" }}
       />
       <input
         id="posEnd"
         type="text"
-        className="form-control"
+        className={`${styles.formControl} ${styles.input} ${styles.posWidth}`}
         value={posEnd}
         onChange={(e) => setPosEnd(e.target.value)}
         placeholder="End Position"
-        style={{ width: "280px" }}
       />
     </div>
   );
