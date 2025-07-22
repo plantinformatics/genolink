cafb3f84d7e2118a2cc34f62e07d149039615735
EC2 Default User <ec2-user@ip-172-31-1-64.ap-southeast-2.compute.internal>
2025-07-22 00:23:49 +0000

    remove multer

--- back/middlewares/errorHandler.js
+++ back/middlewares/errorHandler.js
@@ -1,19 +1,17 @@
-const multer = require('multer');
-const logger = require('./logger');
+const logger = require("./logger");
 
 const errorHandler = (err, req, res, next) => {
-    if (err instanceof multer.MulterError) {
-        logger.error(`MulterError: ${err.message}`);
-                return res.status(400).json({ error: `File upload error: ${err.message}` });
-    } 
+  if (err.response && err.response.data) {
+    logger.error(`API Error: ${err.response.data}`);
+    return res
+      .status(err.response.status || 500)
+      .json({ error: err.response.data.message || "API error occurred." });
+  }
 
-    if (err.response && err.response.data) {
-        logger.error(`API Error: ${err.response.data}`);
-            return res.status(err.response.status || 500).json({ error: err.response.data.message || "API error occurred." });
-    } 
-
-    logger.error(`UnknownError: ${err.message}`);
-        return res.status(err.status || 500).json({ error: err.message || "Unknown server error occurred." });
+  logger.error(`UnknownError: ${err.message}`);
+  return res
+    .status(err.status || 500)
+    .json({ error: err.message || "Unknown server error occurred." });
 };
 
 module.exports = errorHandler;
