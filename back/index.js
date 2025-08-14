99c68eda365546a948b1ddbba9f5f94871706723
Mahdi <mahdi.rahimi@agriculture.vic.gov.au>
2025-08-14 06:18:40 +0000

    Increase Node.js JSON body size limit to prevent 413 errors

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const path = require("path");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "500mb" }));
 app.use(trackRequests);
 app.use("/api", routes);
 
