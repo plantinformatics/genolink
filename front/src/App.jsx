f74bf726ac0de09779f97d4e1e5f3b2f284a94b9
Mahdi <mahdi.rahimi@agriculture.vic.gov.au>
2025-11-02 10:39:38 +0000

    add BASE_PATH as router basename in App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,19 @@
 import Home from "./pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BASE_PATH } from "./config/basePath";
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-  ],
-);
-
-  return (
-      <RouterProvider router={router} />
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+    { basename: BASE_PATH || "/" }
   );
-}
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
