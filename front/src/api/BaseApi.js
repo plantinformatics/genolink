9156b66ef015607ebb1c70e5eb74334d157f9edd
Mahdi <mahdi.rahimi@agriculture.vic.gov.au>
2025-11-02 10:40:37 +0000

    refactor BaseApi formatting

--- front/src/api/BaseApi.js
+++ front/src/api/BaseApi.js
@@ -1,57 +1,57 @@
-import axios from 'axios';
+import axios from "axios";
 
 class BaseApi {
-    constructor(baseUrl, token = null) {
-        this.baseUrl = baseUrl;
-        this.token = token;
+  constructor(baseUrl, token = null) {
+    this.baseUrl = baseUrl;
+    this.token = token;
+  }
+
+  setToken(token) {
+    this.token = token;
+  }
+
+  getToken() {
+    return this.token;
+  }
+
+  async post(endpoint, body = {}, customHeaders = {}, responseType = "json") {
+    try {
+      const response = await axios.post(`${this.baseUrl}${endpoint}`, body, {
+        headers: { ...this.getHeaders(), ...customHeaders },
+        responseType,
+      });
+      return response.data;
+    } catch (error) {
+      console.error(`Error in API call to ${endpoint}:`, error);
+      throw error;
+    }
+  }
+
+  async get(endpoint, params) {
+    try {
+      const response = await axios.get(`${this.baseUrl}${endpoint}`, {
+        headers: this.getHeaders(),
+        params,
+      });
+      return response.data;
+    } catch (error) {
+      console.error(`Error in API call to ${endpoint}:`, error);
+      throw error;
+    }
+  }
+
+  getHeaders() {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json, text/plain, */*",
+    };
+
+    if (this.token) {
+      headers.Authorization = `Bearer ${this.token}`;
     }
 
-    setToken(token) {
-        this.token = token; 
-    }
-
-    getToken() {
-        return this.token;
-    }
-
-    async post(endpoint, body = {}, customHeaders = {}, responseType = 'json') {
-        try {
-            const response = await axios.post(`${this.baseUrl}${endpoint}`, body, {
-                headers: { ...this.getHeaders(), ...customHeaders },
-                responseType,
-            });
-            return response.data
-        } catch (error) {
-            console.error(`Error in API call to ${endpoint}:`, error);
-            throw error;
-        }
-    }
-
-    async get(endpoint, params) {
-        try {
-            const response = await axios.get(`${this.baseUrl}${endpoint}`, {
-                headers: this.getHeaders(),
-                params,
-            });
-            return response.data;
-        } catch (error) {
-            console.error(`Error in API call to ${endpoint}:`, error);
-            throw error;
-        }
-    }
-
-    getHeaders() {
-        const headers = {
-            "Content-Type": "application/json",
-            Accept: "application/json, text/plain, */*",
-        };
-
-        if (this.token) {
-            headers.Authorization = `Bearer ${this.token}`;
-        }
-
-        return headers;
-    }
+    return headers;
+  }
 }
 
 export default BaseApi;
