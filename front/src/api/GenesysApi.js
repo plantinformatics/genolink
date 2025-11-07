5fadb411b2c88268e52ab05aff75f77298c335a6
Mahdi <mahdi.rahimi@agriculture.vic.gov.au>
2025-11-07 03:23:59 +0000

    update Country2Region import to use shared-data alias

--- front/src/api/GenesysApi.js
+++ front/src/api/GenesysApi.js
@@ -2,7 +2,7 @@ import BaseApi from "./BaseApi";
 import { genesysServer } from "../config/apiConfig";
 import oidcConfig from "../config/oidcConfig";
 import { genolinkInternalApi } from "../pages/Home";
-import country2Region from "../../shared-data/Country2Region.json";
+import country2Region from "shared-data/Country2Region.json";
 import {
   setInstituteCheckedBoxes,
   setActiveFilters,
