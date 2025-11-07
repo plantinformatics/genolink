680cab4e0aafd095f21df230cd739929866cdbe1
Mahdi <mahdi.rahimi@agriculture.vic.gov.au>
2025-11-07 04:00:44 +0000

    remove unused GIGWA_SERVERS and GERMINATE_SERVER from appConfig

--- back/config/appConfig.js
+++ back/config/appConfig.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  gigwaServers: JSON.parse(process.env.GIGWA_SERVERS || "{}"),
-  germinateServer: process.env.GERMINATE_SERVER,
   genolinkServer: process.env.GENOLINK_SERVER,
   genesysServer: process.env.GENESYS_SERVER,
   genolinkServerPort: process.env.GENOLINK_SERVER_PORT || 4000,
